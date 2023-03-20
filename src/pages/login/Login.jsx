f04a51263443fb3ea80d66e6d5de958229a334ca
Your Name <you@example.com>
2023-03-20 15:49:42 +0100

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import css from './Login.module.css'
+import LoginImg from '../../images/charming-successful-young-female-yellow-autumn-coat-smiling-broadly-holding-hands-crossed-body-confident-self-assured-pose-standing-delighted-happy-red-background-removebg-preview.png';
+import Google from '../../images/R.png';
+import Apple from '../../images/R (1).png';
+import {Link} from 'react-router-dom'
+const Login = () => {
+  return (
+    <div className={css.container}>
+     <div className={css.header}>
+        <span>F</span>
+     </div>
+     <div className={css.login}>
+        <div className={css.left}>
+            <img src={LoginImg} alt="" />
+            <div className={css.background}>
+             <div className={css.bgtop}>
+                <p>Fund has given my business the needed boost to cover a large market with proper review from expert on the platform</p>
+             </div>
+             <div className={css.bgbottom}>
+                <div className={css.ceo}>
+                    <p>Tope Adesuwa</p>
+                    <p>CEO of Milling Fags Limited</p>
+                </div>
+                <div className={css.rating}>
+                    <span><i class="fa-solid fa-star"></i></span>
+                    <span><i class="fa-solid fa-star"></i></span>
+                    <span><i class="fa-solid fa-star"></i></span>
+                    <span><i class="fa-solid fa-star"></i></span>
+                    <span><i class="fa-solid fa-star"></i></span>
+                </div>
+             </div>
+            </div>
+        </div>
+        <div className={css.right}>
+            <h1>Get Started</h1>
+            <form className={css.form}>
+                <div className={css.form_group}>
+                    <label>Email</label>
+                    <input type="text" placeholder='Email'/>
+                </div>
+                <div className={css.form_group}>
+                    <label>Password</label>
+                    <input type="text" placeholder='Password...'/>
+                </div>
+                <div className={css.forget}>
+                    <p>Forgot Password</p>
+                </div>
+                <div className={css.button}>
+                    <button>Login</button>
+                </div>
+                <div className={css.or}>
+                <hr /><span>Or</span><hr />
+                </div>
+                <div className={css.button}>
+                    <button className={css.google}>Continue With Google</button>
+                    <img src={Google} alt="" />
+                </div>
+                <div className={css.button}>
+                    <button className={css.apple}>Continue With Apple</button>
+                    <img src={Apple} alt="" />
+                </div>
+                <div className={css.signup}>
+                    <p>Don't have a Fund account? <Link to="/">Signup</Link> </p>
+                </div>
+            </form>
+        </div>
+     </div>
+    </div>
+  )
+}
+
+export default Login
